228a026204fb1e93dbddf15ee60c0dd30bde7548
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-02-21 15:13:04 -0300

    Inicial

---
+++ src/main/java/com/cluprogramador/HolaMundoMaven.java
@@ -0,0 +1,24 @@
+package com.cluprogramador;
+
+/*
+*Nombre de clase = HolaMundoMaven
+*Descripción = Clase Principal del Proyecto
+*@autor club del Programador
+*@version 1.0
+*/
+
+public class HolaMundoMaven {
+
+	/**
+	 * Método = main
+	 * Descripción = metodo estatico de main principal.
+	 * @param args = tipo array de String
+	 * @return void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hola Mundo Maven!...");  //Impresion en Consola
+         System.out.println("Bienvenido al Club del Programador!..."); //Impresion en Consola
+	}
+
+}
