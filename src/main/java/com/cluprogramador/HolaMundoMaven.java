0d31f8b1973850050dca8d0c9027864fe4e38316
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-03 12:20:40 -0300

    Test

--- src/main/java/com/cluprogramador/HolaMundoMaven.java
+++ src/main/java/com/cluprogramador/HolaMundoMaven.java
@@ -18,11 +18,11 @@ public class HolaMundoMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hola Mundo Maven!...");  //Impresion en Consola
-         System.out.println("Bienvenido al Club del Programador!..."); //Impresion en Consola 
-		 System.out.println("Compilacion desde MVN"); //Impresion en Consola
-         System.out.println("Tercer Commit");
-	     System.out.println("Correccion de Error");
-		 System.out.println("Correccion de Error");
+         // System.out.println("Bienvenido al Club del Programador!..."); //Impresion en Consola 
+		 // System.out.println("Compilacion desde MVN"); //Impresion en Consola
+		 // System.out.println("Tercer Commit");
+		 // System.out.println("Correccion de Error");
+		 // System.out.println("Correccion de Error");
 		
 	 }
 }
