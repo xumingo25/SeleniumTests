daa91a0a6171969fa04363cbe0ae90f307e9ea71
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-08 11:27:50 -0300

    Test Copia Remota

--- src/main/java/com/cluprogramador/HolaMundoMaven.java
+++ src/main/java/com/cluprogramador/HolaMundoMaven.java
@@ -18,8 +18,7 @@ public class HolaMundoMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hola Mundo Maven!...");  //Impresion en Consola
-         // System.out.println("Bienvenido al Club del Programador!..."); //Impresion en Consola 
-		 // comentarios+
-		 // comentarios
+		 
+
 	 }
 }
