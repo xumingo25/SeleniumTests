78b5e7a7321b8821209d27e93a36a4eda76d73c4
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-08 12:13:14 -0300

    Test Auto Deploy

--- src/main/java/com/cluprogramador/HolaMundoMaven.java
+++ src/main/java/com/cluprogramador/HolaMundoMaven.java
@@ -18,7 +18,5 @@ public class HolaMundoMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hola Mundo Maven!...");  //Impresion en Consola
-		 
-
 	 }
 }
